dcb2db8619f74ab03994f9757bf5ed6135ebc22b
rafa3lsantos <rafaelsantosdoamaral07@gmail.com>
2024-10-23 13:37:43 -0300

    Projeto Finalizado

---
+++ vue-app/src/components/BurgerForm.vue
@@ -0,0 +1,183 @@
+<template>
+    <div>
+        <Message :msg="msg" v-show="msg"/>
+        <div>
+            <form id="burger-form" @submit="createBurger">
+                <div class="input-container">
+                    <label for="nome">Nome do Cliente: </label>
+                    <input type="text" id="nome" name="nome" v-model="nome" placeholder="Digite o seu nome">
+                </div>
+                <div class="input-container">
+                    <label for="pao">Escolha o pão: </label>
+                    <select name="pao" id="pao" v-model="pao">
+                        <option value="">Selecione seu pao</option>
+                        <option v-for="pao in paes" :key="pao.id" :value="pao.tipo">
+                            {{ pao.tipo }}
+                        </option>
+                    </select>
+                </div>
+                <div class="input-container">
+                    <label for="carne">Escolha o carne do seu burger: </label>
+                    <select name="carne" id="carne" v-model="carne">
+                        <option value="">Selecione o tipo de carne</option>
+                        <option v-for="carne in carnes" :key="carne.id" :value="carne.tipo">
+                            {{ carne.tipo }}
+                        </option>                    
+                    </select>
+                </div>
+                <div id="opcionais-container" class="input-container">
+                    <label id="opcionais-title" for="opcionais">Selecione os opcionais: </label>
+                    <div class="checkbox-container" v-for="opcional in opcionaisdata" :key="opcional.id">
+                        <input type="checkbox" name="opcionais" v-model="opcionais" :value="opcional.tipo">
+                        <span>{{ opcional.tipo }}</span>
+                    </div>
+                </div>
+                <div class="input-container">
+                    <input type="submit" class="submit-btn" value="Criar meu burger!">
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Message from './Message.vue';
+
+    export default {
+        name: "BurgerForm",
+        data() {
+            return {
+            paes: null,
+            carnes: null,
+            opcionaisdata: null,
+            nome: null,
+            pao: null,
+            carne: null,
+            opcionais: [],
+            status: "Solicitado",
+            msg: null
+            }
+        },
+        methods: {
+            async getIngredients() {
+
+                const req = await fetch("http://localhost:3001/ingredientes")
+                const data = await req.json();
+
+                this.paes = data.paes;
+                this.carnes = data.carnes;
+                this.opcionaisdata = data.opcionais
+
+            },
+            async createBurger(e) {
+                
+                e.preventDefault();
+
+                const data = {
+                    nome: this.nome,
+                    carne: this.carne,
+                    pao: this.pao,
+                    opcionais: Array.from(this.opcionais),
+                    status: "Solicitado"
+                }
+
+                const dataJson = JSON.stringify(data);
+
+                const req = await fetch("http://localhost:3001/burgers", {
+                   method: "POST",
+                   heaers: { "Content-Type": "application/json" },
+                   body: dataJson 
+                });
+
+                const res = await req.json();
+
+                // colocar uma msg de sistema
+                this.msg = `Pedido ${res.id} realizado com sucesso`
+
+                // limpar msg
+                setTimeout(() => this.msg = "", 3000);
+
+                // limpar os campos
+                this.nome = "";
+                this.carne = "";
+                this.pao = "";
+                this.opcionais = [];
+
+            }
+        },
+        mounted() {
+            this.getIngredients()
+        },
+        components: {
+            Message
+        }
+    }
+</script>
+
+<style scoped>
+    #burger-form {
+        max-width: 400px;
+        margin: 0 auto;
+    }
+
+    .input-container {
+        display: flex;
+        flex-direction: column;
+        margin-bottom: 20px;
+    }
+
+    label {
+        font-weight: bold;
+        margin-bottom: 15px;
+        color: #222;
+        padding: 5px 10px;
+        border-left: 4px solid #FCBA03;
+    }
+
+    input, select {
+        padding: 5px 10px;
+        width: 300px ;
+    }
+
+    #opcionais-container {
+        flex-direction: row;
+        flex-wrap: wrap;
+    }
+
+    #opcionais-title {
+        width: 100%;
+    }
+
+    .checkbox-container {
+        display: flex;
+        align-items: flex-start;
+        width: 50%;
+        margin-bottom: 20px;
+    }
+
+    .checkbox-container span,
+    .checkbox-container input {
+        width: auto;
+    }
+
+    .checkbox-container span {
+        margin-left: 6px;
+        font-weight: bold;
+    }
+
+    .submit-btn {
+        background-color: #222;
+        color: #FCBA03;
+        font-weight: bold;
+        border: 2px solid #222;
+        padding: 10px;
+        margin: 0 auto;
+        cursor: pointer;
+        transition: .5s;
+    }
+
+    .submit-btn:hover {
+        background-color: #FCBA03;
+        color: #222;
+    }
+</style>
--- vue-app/src/views/HomeView.vue
+++ vue-app/src/views/HomeView.vue
@@ -1,10 +1,21 @@
 <template>
-  <div class="home">
-    <h1>HOME</h1>
-  </div>
+  <Banner />
+  <div class="main-container">
+    <h1>Monte o seu Burger:</h1>
+    <BurgerForm />
+</div>
 </template>
 
 <script>
+  import Banner from "../components/Banner.vue"
+  import BurgerForm from "@/components/BurgerForm.vue";
 
+  export default {
+    name: "Home",
+    components: {
+      Banner,
+      BurgerForm
+    }
+  }
 </script>
 
